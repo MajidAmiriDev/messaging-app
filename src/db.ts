a4b6144900a15b2f7f2a55e66e5fc5982cafa5cc
root <root@Majid>
2024-09-21 16:44:15 +0330

    9th step improve db

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,23 @@
 import mongoose from 'mongoose';
 
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/messaging-app';
+
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/messaging-app', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('MongoDB connection error', err);
+    let retries = 5;
+
+    while (retries) {
+        try {
+            await mongoose.connect(dbURI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            console.log('MongoDB connected');
+            break; // اتصال موفقیت‌آمیز بود، حلقه را ترک کنید
+        } catch (err) {
+            retries -= 1;
+            console.error('MongoDB connection error', err);
+            console.log(`Retries left: ${retries}`);
+            await new Promise(res => setTimeout(res, 5000)); // 5 ثانیه صبر کنید
+        }
     }
 };
